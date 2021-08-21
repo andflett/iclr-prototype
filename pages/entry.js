e56d0153a5cd12aa3319a36bb687197f93038ecd
Andrew Flett <andrflett@gmail.com>
2021-08-21 09:30:09 +0100

    WIP

--- pages/entry.js
+++ pages/entry.js
@@ -2,12 +2,26 @@ import ChakraAwesome from '../components/ChakraAwesome'
 import { CaseHeader } from '../components/CaseHeader'
 import { FloatingNav } from '../components/FloatingNav'
 
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
-      <CaseHeader showDownload={true} />
+      <CaseHeader showDownload={true}>
+        <Button
+          pl="3"
+          pr="6"
+          as="a"
+          href="/genie"
+          _hover={{ bg: "purple.600" }}
+          bg="purple.500"
+          fontWeight="500"
+          color="white"
+          fontSize="sm">
+          <ChakraAwesome fontSize="0.9rem" color="white" icon={['fai', 'lamp']} mr="0.75rem" />
+          Case Genie
+        </Button>
+      </CaseHeader>
       <FloatingNav />
       <Box bg="white" textAlign="center" py='6'>
         <Box py="20">
